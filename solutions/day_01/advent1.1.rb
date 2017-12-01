c76a0329085ee53de733003969f83fee682afb4c
Sean Handley <sean.handley@gmail.com>
2017-12-01 08:53:31 +0000

    Day 1 solution.

--- solutions/day_01/advent1.1.rb
+++ solutions/day_01/advent1.1.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
 
-puts "Hello, world!"
+def input
+  @input ||= STDIN.gets.chars.map(&:to_i)
+end
+
+def calculate_captcha(offset)
+  pairs = input.each_with_index.map do |e, i|
+    index = (i + offset) % input.length
+    e == input[index] ? e : 0
+  end
+  pairs.reduce :+
+end
+
+if __FILE__ == $0
+  puts calculate_captcha(1)
+end
---
+++ solutions/day_01/advent1.2.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require_relative "./advent1.1"
+
+puts calculate_captcha(input.count / 2)
