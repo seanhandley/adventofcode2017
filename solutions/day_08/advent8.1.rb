e01d26839d9da45e905006118380c35049e1fa2b
Sean Handley <sean.handley@gmail.com>
2017-12-09 12:14:34 +0000

    Day 8 solution.

---
+++ solutions/day_08/advent8.1.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+def instructions
+  @instructions ||= STDIN.read.split("\n")
+end
+
+def registers
+  @registers ||= {}
+end
+
+def parse_and_execute(instruction)
+  # kd dec -37 if gm <= 9
+  a, b = instruction.split("if")
+  register, operation, value  = a.strip.split
+  cond_reg, cond_op, cond_val = b.strip.split
+
+  if (registers[cond_reg] ||= 0).send(cond_op.to_sym, cond_val.to_i)
+    registers[register] ||= 0
+    case operation
+    when "dec"
+      registers[register] -= value.to_i
+    when "inc"
+      registers[register] += value.to_i
+    end
+  end
+end
+
+def run
+  instructions.each do |instruction|
+    parse_and_execute(instruction)
+  end
+  registers.values.max
+end
+
+if __FILE__ == $0
+  p run
+end
---
+++ solutions/day_08/advent8.2.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+@highest = 0
+
+def instructions
+  @instructions ||= STDIN.read.split("\n")
+end
+
+def registers
+  @registers ||= {}
+end
+
+def parse_and_execute(instruction)
+  a, b = instruction.split("if")
+  register, operation, value  = a.strip.split
+  cond_reg, cond_op, cond_val = b.strip.split
+
+  if (registers[cond_reg] ||= 0).send(cond_op.to_sym, cond_val.to_i)
+    registers[register] ||= 0
+    case operation
+    when "dec"
+      registers[register] -= value.to_i
+    when "inc"
+      registers[register] += value.to_i
+    end
+    @highest = registers[register] if registers[register] > @highest
+  end
+end
+
+def run
+  instructions.each do |instruction|
+    parse_and_execute(instruction)
+  end
+  @highest
+end
+
+if __FILE__ == $0
+  p run
+end
